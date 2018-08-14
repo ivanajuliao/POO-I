ec04e3230220fd3b1756d99159e33964b1dadb14
Ivana <ivana_juliao@yahoo.com.br>
2018-08-14 08:05:13 -0300

    Criação da classe conta poupança

--- SistemaContaBancaria/src/banco/ContaPoupança.java
+++ SistemaContaBancaria/src/banco/ContaPoupança.java
@@ -11,4 +11,45 @@ package banco;
  */
 public class ContaPoupança {
     
+   private int numConta;
+   private String nomeTitular;
+   private double saldo;
+   private int agencia;
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+   
+   public void depositar(double valor){
+       this.saldo += valor;
+   }
+   
+   public void sacar(double valor){
+       this.saldo -= valor;
+   }
+   
+   public void imprirSaldo(){
+       System.out.println("Prezado, " + this.nomeTitular);
+        System.out.println("Saldo: R$" + this.saldo);
+   }
 }
