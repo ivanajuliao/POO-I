1753187a3b8808942d871d82b060c7d480e0693e
Ivana <ivana_juliao@yahoo.com.br>
2018-08-14 07:32:03 -0300

    Criação das funções sacar/ depositar/ imprimirSaldo

--- SistemaContaBancaria/src/banco/ContaCorrente.java
+++ SistemaContaBancaria/src/banco/ContaCorrente.java
@@ -11,4 +11,59 @@ package banco;
  */
 public class ContaCorrente {
     
+   private int numConta;
+   private int agencia;
+   private String nomeTitular;
+   private double saldo;
+   private double limite;
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+    
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+    
+    public void imprimirSaldo() {
+        System.out.println("Prezado, " + this.nomeTitular);
+        System.out.println("Saldo: R$" + this.saldo);
+        System.out.println("Limite: R$" + this.limite);
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+   
+   
+    
+   
 }
