b156e672ddbb196d901187c07207ef1ade133064
Ivana A. Julião <ivana_juliao@yahoo.com.br>
2018-08-21 11:29:18 -0300

    JogoPerguntasMatematicas

---
+++ JogoPerguntasMatematicas/src/jogo/Jogo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo;
+
+/**
+ *
+ * @author 20131bsi0173
+ */
+public class Jogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        QuestaoSoma novaQuestao = new QuestaoSoma();
+        QuestaoSubitracao novaQuestaoSub = new QuestaoSubitracao();
+        QuestaoMultiplicacao novaQuestaoMult = new QuestaoMultiplicacao();
+        
+        novaQuestao.criaPergunta();
+        novaQuestao.verificaResposta();
+        novaQuestao.exibirResultado();
+        
+        novaQuestaoSub.criaPergunta();
+        novaQuestaoSub.verificaResposta();
+        novaQuestaoSub.exibirResultado();
+        
+        novaQuestaoMult.criaPergunta();
+        novaQuestaoMult.verificaResposta();
+        novaQuestaoMult.exibirResultado();
+    }
+    
+}
---
+++ JogoPerguntasMatematicas/src/jogo/QuestaoSoma.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo;
+
+import java.util.concurrent.ThreadLocalRandom;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 20131bsi0173
+ */
+public class QuestaoSoma {
+    private int operador1 = ThreadLocalRandom.current().nextInt(0,100);
+    private int operador2 = ThreadLocalRandom.current().nextInt(0, 20);
+    private int pontos;
+    private int resposta;
+    private String respostaS;
+    private boolean resultado = false;
+        
+    public QuestaoSoma(){}
+    
+    public void criaPergunta(){
+        this.respostaS = JOptionPane.showInputDialog("Quanto é "+ this.operador1 + " + "+ this.operador2);    
+    }
+    public void verificaResposta(){
+        this.resposta = Integer.parseInt(this.respostaS);
+        if( this.operador1 + this.operador2 == this.resposta){
+            this.resultado = true;
+        }  
+    }
+    
+    public void exibirResultado(){
+        if(this.resultado){
+            JOptionPane.showMessageDialog(null, "Você acertou!");
+        } else{
+            JOptionPane.showMessageDialog(null, "Você errou");
+        } 
+    }
+    
+}
