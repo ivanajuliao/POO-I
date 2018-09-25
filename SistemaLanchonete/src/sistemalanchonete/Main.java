443da099ff2d7d10e597e1f210396307e46c8233
Ivana A. Julião <ivana_juliao@yahoo.com.br>
2018-09-25 10:44:48 -0300

    Update Main.java

--- SistemaLanchonete/src/sistemalanchonete/Main.java
+++ SistemaLanchonete/src/sistemalanchonete/Main.java
@@ -16,6 +16,30 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Pedido novoPedido = new Pedido(0);
+        ItemPedido item1 = new ItemPedido(0);
+        ItemPedido item2 = new ItemPedido(0);
+
+        
+        Hamburguer novoBurguer = new Hamburguer(001, "Vegetariano Especial", 18.00);
+        TipoSuco sabor = new TipoSuco("Laranja");
+        Suco novoSuco = new Suco(002, 5.00);
+        novoSuco.addSabores(sabor);
+        
+        Refrigerante novoRefri = new Refrigerante(003, 9.00);
+        
+        item1.addItemComida(novoBurguer);
+        item1.addItemBebida(novoRefri);
+        item1.setValorParcial(novoBurguer.getValor()+ novoRefri.getValor());
+        
+        item2.addItemBebida(novoSuco);
+        item2.setValorParcial(novoSuco.getValor());
+        
+        novoPedido.adicionarItemPedido(item1);
+        novoPedido.adicionarItemPedido(item2);
+        
+        novoPedido.listarPedidos();
+        
     }
     
 }
