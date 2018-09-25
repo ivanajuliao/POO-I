bbb52cd748b035941e7bd7fa82842fd7c0bdd257
Ivana A. Julião <ivana_juliao@yahoo.com.br>
2018-09-25 11:25:33 -0300

    Add files via upload

---
+++ SistemaDigitalizacaoArquivos/src/conf/sistemadigitalizacaoarquivos/Aluno.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadigitalizacaoarquivos;
+
+/**
+ *
+ * @author 20131bsi0173
+ */
+public class Aluno {
+    
+    private String nome;
+    private String matricula;
+    private Curso curso;
+
+    public Aluno(String nome, String matricula, Curso curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+    
+    public void alterarAluno(String nome, String matricula, Curso curso) {
+        setNome(nome);
+        setMatricula(matricula);
+        setCurso(curso);
+    }
+    
+    public void visualizarAluno() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("Matrícula" + getMatricula());
+        System.out.println("Curso:" + getCurso());
+        
+    }
+    
+    public void excluirAluno() {
+        
+    }
+ 
+}
---
+++ SistemaDigitalizacaoArquivos/src/conf/sistemadigitalizacaoarquivos/Curso.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadigitalizacaoarquivos;
+
+/**
+ *
+ * @author 20131bsi0173
+ */
+public class Curso {
+    
+    private int codCurso;
+    private String nome;
+
+    public Curso(int codCurso, String nome) {
+        this.codCurso = codCurso;
+        this.nome = nome;
+    }
+
+    public int getCodCurso() {
+        return codCurso;
+    }
+
+    public void setCodCurso(int codCurso) {
+        this.codCurso = codCurso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+     public void alterarCurso(String nome, int codCurso) {
+        setNome(nome);
+        setCodCurso(codCurso);
+    }
+    
+    public void visualizarCurso() {
+        System.out.println("Curso: " + getNome());
+        System.out.println("Código:" + getCodCurso());
+        
+    }
+    
+    public void excluirCurso() {
+        
+    }
+}
