873354e95817419e287440adaf800713beefe8fd
Ivana <ivana_juliao@yahoo.com.br>
2018-10-15 12:07:27 -0300

    Setor serializado

---
+++ SistemaDigitalizacaoArquivos/src/java/persistencia/Serializado/SetorSerializado.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia.Serializado;
+
+import java.io.IOException;
+import java.io.Serializable;
+import modelo.Funcionario;
+import modelo.Setor;
+
+/**
+ *
+ * @author Ivana
+ */
+public class SetorSerializado implements Serializable{
+    
+    persistencia.Serializado.PersistenciaSerializada persistencia = new PersistenciaSerializada();
+    Setor setor;
+    
+    public void salvarSetorSerializado(Setor setor) throws IOException {
+        persistencia.salvarObjeto(setor);
+    }
+    
+    public Setor recuperarSetorSerializado() throws IOException, ClassNotFoundException {
+        Setor setorRecuperado = (Setor) persistencia.recuperarObjeto(Setor.class);
+        return setorRecuperado;
+    }
+    
+    public void alterarSetor(String nome, int codSetor, Funcionario coordenador) {
+        this.setor.setNomeSetor(nome);
+        this.setor.setCodSetor(codSetor);
+        this.setor.setCoordenador(coordenador);
+    }
+    
+    public void visualizarSetor() {
+        System.out.println("Setor: " + this.setor.getNomeSetor());
+        System.out.println("Código:" + this.setor.getCodSetor());
+        System.out.println("Coordenador:"+ this.setor.getCoordenador());
+        
+    }
+    
+    public void excluirSetor() {
+        
+    }
+}
+
